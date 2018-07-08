859fe6f324a8e60a78fa4b76768b5c24116ba4f6
Rebecca Carroll <rcarroll@fullsail.com>
2018-07-08 15:26:15 -0400

    Updated graphics. Added p1wins graphic

--- app/src/main/java/com/example/rebeccacarroll/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/rebeccacarroll/tictactoe/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.rebeccacarroll.tictactoe;
 
 import android.content.pm.ActivityInfo;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int[] game = {1,-1,1,-1,1,1,-1,1,1};
+    private int[] game =    { 0,0,0,
+                              0,0,0,
+                              0,0,0};
+
     private int[] tiles = {R.id.tile0, R.id.tile1,R.id.tile2,R.id.tile3,R.id.tile4,R.id.tile5,R.id.tile6,R.id.tile7,R.id.tile8};
+    public int player = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +23,46 @@ public class MainActivity extends AppCompatActivity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
 
         setContentView(R.layout.activity_main);
-        gameSetup();
+
+        init();
+    }
+    private void init(){
+        updateBoard();
+
+        for(int i: tiles){
+            ImageView tiles  = findViewById(i);
+            tiles.setOnClickListener(tileListener);
+        }
+    }
+    private void move(){
+        //toggle player
+        checkWin();
+
+        ImageView v = findViewById(R.id.player);
+        if(player == 1){
+            player = 0;
+            v.setImageResource(R.drawable.player1);
+        }else{
+            player = 1;
+            v.setImageResource(R.drawable.player2);
+        }
 
     }
-    
-    protected void gameSetup(){
+    private void checkWin(){
+
+    }
+    public View.OnClickListener tileListener  =  new View.OnClickListener() {
+        public void onClick(View v) {
+            ImageView tile = (ImageView) v;
+            if(player == 1){
+                tile.setImageResource(R.drawable.x);
+            }else{
+                tile.setImageResource(R.drawable.o);
+            }
+            move();
+        }
+    };
+    protected void updateBoard(){
         //cycles through tiles to match them to match them to game array
         for (int i=0;i<tiles.length; i++) {
             ImageView view  = findViewById(tiles[i]);
